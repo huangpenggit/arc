aade60064f92f1f565a11d01803cc5a1b3191a8d
may <2320158601@qq.com>
2018-07-15 21:22:41 +0800

    0.1

---
+++ web/website-https/src/main/java/com/del/express/ExpressApplication.java
@@ -0,0 +1,12 @@
+package com.del.express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpressApplication.class, args);
+    }
+}
